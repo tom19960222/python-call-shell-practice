f724ba62c7f6dd9c1f6215ba3382af43808cb20b
爆頭專家 <tom19960222@gmail.com>
2020-05-24 00:41:53 +0800

    增加說明

--- main.py
+++ main.py
@@ -2,9 +2,10 @@ import tkinter as tk
 import subprocess
 import threading
 
+# 先改成你要執行的程式
 script_path_list = [
   "/Users/ikaros/Documents/practice/python-tk/sleep.sh",
-  "/Users/ikaros/Documents/practice/python-tk/sleep2.sh",
+  ["/Users/ikaros/Documents/practice/python-tk/sleep2.sh", 'Hello!']
 ]
 
 def execute(path):
@@ -15,13 +16,13 @@ def execute(path):
 def process_script():
   for script_path in script_path_list:
     header_label.configure(text=f'正在處理 {script_path}')
-    print(execute(script_path))
+    print(execute(script_path)) # 輸出結果會印在 Console 上
   header_label.configure(text='處理完畢')
-  calculate_btn.configure(state=tk.NORMAL)
+  go_btn.configure(state=tk.NORMAL)
 
   
 def btn_click():
-  calculate_btn.configure(state=tk.DISABLED)
+  go_btn.configure(state=tk.DISABLED)
   t = threading.Thread(target=process_script)
   t.start()
 
@@ -32,7 +33,7 @@ window.configure(background='white')
 header_label = tk.Label(window, text='等待開始', width=100)
 header_label.pack()
 
-calculate_btn = tk.Button(window, text='GO', command=btn_click)
-calculate_btn.pack()
+go_btn = tk.Button(window, text='GO', command=btn_click)
+go_btn.pack()
 
 window.mainloop()
